04cb66ee1f62692afb19fb0b5755ea454d2c1346
Frank Rosner <frank@fam-rosner.de>
2018-06-30 08:09:45 +0200

    Delete backend.tf

--- backend/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "travisterraformtest-195499643157-eu-central-1"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "travisterraformtest-195499643157-eu-central-1"
-  }
-}
